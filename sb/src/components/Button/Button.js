0c64f7c7122228af7b23d48d07ce4b369669b717
Maxssobolev <himax108@mail.ru>
2021-12-18 23:30:55 +0300

    button

--- sb/src/components/Button/Button.js
+++ sb/src/components/Button/Button.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { ReactComponent as RightArrow } from '../../assets/img/arrow-right.svg';
+
+
 
 export default function Button({ type, text, __class }) {
 
     return (
         <button type="button" className={`button ${__class || ''}`}>
             {text}
+            {type === 'readmore' && <RightArrow />}
         </button>
     )
 }
