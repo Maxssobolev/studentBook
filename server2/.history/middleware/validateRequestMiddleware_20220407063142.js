bad4ec7e50f8767ce02d0dc4fa0dcdd135948fc9
Maxssobolev <himax108@mail.ru>
2022-04-07 20:29:45 +0300

    delete local history from git

--- server2/.history/middleware/validateRequestMiddleware_20220407063142.js
+++
@@ -1,9 +0,0 @@
-const { validationResult } = require('express-validator');
-const ApiError = require("../error/ApiError")
-
-export function validateRequestMiddleware(req, res, next) {
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-        next(ApiError.badFormData(errors.array()))
-    }
-}
